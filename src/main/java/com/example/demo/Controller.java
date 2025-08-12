293e19ca8448b324783790262458ed128b5876dd
pranaytejach <chowkepranay@gmail.com>
2025-08-12 15:16:39 +0530

    second commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,9 +3,9 @@ package com.example.demo;
 public class Controller {
 	public String greet() {
 <<<<<<< HEAD
-		return "hello from pranay and rutish";
+		return "hello from pranay and rutish 2";
 =======
-		return "hello from rutish";
+		return "hello from rutish ";
 
 >>>>>>> 8461b686e5a33f1fedcabd899fb8d5bb63fcb56f
 	}
