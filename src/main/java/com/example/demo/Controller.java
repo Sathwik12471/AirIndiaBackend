cb10ac2c76a6ede9384917b4bda9e5224198a5d0
pranaytej41 <pranaytej41@gmail.com>
2025-08-12 15:23:51 +0530

    Pranay (#2) (#5)
    * changes
    
    * changes for rutish
    
    ---------
    
    Co-authored-by: pranaytejach <chowkepranay@gmail.com>

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,12 +2,6 @@ package com.example.demo;
 
 public class Controller {
 	public String greet() {
-<<<<<<< HEAD
-		return "hello from pranay and rutish 2";
-=======
-		return "hello from rutish ";
 
->>>>>>> 8461b686e5a33f1fedcabd899fb8d5bb63fcb56f
-	}
 
 }
